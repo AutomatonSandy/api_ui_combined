11d7272a3d80184b8fb2ecea2d65ec06d788cf10
Sandipan Bhattacharjee <sandipanbhattacharjee1981@gmail.com>
2026-02-18 10:09:20 -0800

    Trying to ascertain tests are working in remote machine

--- src/test/java/base/BaseUITest.java
+++ src/test/java/base/BaseUITest.java
@@ -7,15 +7,23 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
 
+import java.net.UnknownHostException;
+
 public class BaseUITest {
         protected WebDriver driver;
 
         @Parameters("browser")
         @BeforeMethod
-        public void setup(String browser) {
+        public void setup(String browser){
             driver = DriverFactory.initDriver(browser);
             driver.get("https://the-internet.herokuapp.com/");
             Assert.assertTrue(driver.getTitle().equals("The Internet"));
+            try {
+                System.out.println("Host: " +
+                        java.net.InetAddress.getLocalHost().getHostName());
+            } catch (UnknownHostException e) {
+                throw new RuntimeException(e);
+            }
         }
 
         @AfterMethod
